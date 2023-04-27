847b06ff2869d9f8c66af2cfeb73296184c2a8ae
Amogh Kini <amoghkini@gmail.com>
2023-04-27 23:03:54 +0530

    Factory method implementation

--- src/config/config.py
+++ src/config/config.py
@@ -52,7 +52,7 @@ class BaseConfig(object):
     # flask                                                                  #
     ##########################################################################
     
-    DEBUG = True if get_env() == 'dev' else False
+    DEBUG = False
     SECRET_KEY = "THIS IS SECRET KEY"  
 
 
@@ -83,11 +83,11 @@ class ProdConfig(BaseConfig):
     # database                                                               #
     ##########################################################################
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'.format(
-        user=os.environ.get('FLASK_DATABASE_USER', 'postgres'),
-        password=os.environ.get('FLASK_DATABASE_PASSWORD', 'root'),
-        host=os.environ.get('FLASK_DATABASE_HOST', '127.0.0.1'),
+        user=os.environ.get('POSTGRES_USER', 'postgres'),
+        password=os.environ.get('POSTGRES_PASSWORD', 'root'),
+        host=os.environ.get('FLASK_DATABASE_HOST', 'localhost'),
         port=os.environ.get('FLASK_DATABASE_PORT', 5432),
-        db_name=os.environ.get('FLASK_DATABASE_NAME', 'postgres'),
+        db_name=os.environ.get('POSTGRES_DB', 'postgres'),
     )
 
 
@@ -103,9 +103,9 @@ class DevConfig(BaseConfig):
     ##########################################################################
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'.format(
-        user=os.environ.get('FLASK_DATABASE_USER', 'flask_api'),
-        password=os.environ.get('FLASK_DATABASE_PASSWORD', 'flask_api'),
-        host=os.environ.get('FLASK_DATABASE_HOST', '127.0.0.1'),
+        user=os.environ.get('POSTGRES_USER', 'postgres'),
+        password=os.environ.get('POSTGRES_PASSWORD', 'root'),
+        host=os.environ.get('FLASK_DATABASE_HOST', 'localhost'),
         port=os.environ.get('FLASK_DATABASE_PORT', 5432),
-        db_name=os.environ.get('FLASK_DATABASE_NAME', 'flask_api'),
+        db_name=os.environ.get('POSTGRES_DB', 'postgres'),
     )
--- src/restapis/password_reset.py
+++ src/restapis/password_reset.py
@@ -30,35 +30,6 @@ class PasswordResetAPI(MethodView):
                                           'message': str(e),
                                           'data': None}), 404)
 
-    def post(self,token):
-        
-        email = UserMethods.decode_token(token)
-        print(email)
-        
-        if not email:
-            return make_response(jsonify({'status': 'error',
-                                          'message': 'The password reset link is either expired or invalid.',
-                                          'data': None}), 404)
-        query = text(
-            'SELECT user_id, email_id, first_name, account_creation_date, role_id FROM backtest.users WHERE email_id = :email_id')
-        result = g.session.execute(query, {'email_id': email})
-
-        # Retrieve the first row returned by the query
-        user = result.fetchone()
-
-        data = request.get_json()
-        print(data)
-
-        if user:
-            # verify the password
-            # Save the updated password in the database.
-            return make_response(jsonify({'status': 'success',
-                                          'message': 'Password changed successfully',
-                                          'data': []}), 200)
-        else:
-            return make_response(jsonify({'status': 'error',
-                                          'message': 'The user is not registered in the system.',
-                                          'data': None}), 404)
 
     def post(self, token):
         # Parse the JSON request data
